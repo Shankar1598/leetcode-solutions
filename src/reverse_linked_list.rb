34cb519794786371ef7774c47a9af7da87694fe7
Shankar T <shankar15498@gmail.com>
2023-01-06 09:21:56 +0530

    bug fix and completing reverse linked list

--- src/reverse_linked_list.rb
+++ src/reverse_linked_list.rb
@@ -1,3 +1,10 @@
+=begin
+The normal solution seems to be faster than the recursive one, and more memory optimized as well
+Solution 4 seems to be the best
+=end
+
+
+
 # Definition for singly-linked list.
 class ListNode
     attr_accessor :val, :next
@@ -9,18 +16,78 @@ class ListNode
       puts "#{self.val}"
       self.next&.ps
     end
+    def to_s
+      "ListNode: #{val}"
+    end
 end
 # @param {ListNode} head
 # @return {ListNode}
 def reverse_list(head)
-  if head.next
-    tail = reverse_list(head.next)
-    puts "tail returned: #{tail.val}. Current head: #{head.val}"
-    tail.next = head
-    return tail.next
+  # Solution 1
+  # new_head, _new_tail = reverse_list_recursive(head)
+  # return new_head
+
+  # Solution 2
+  # array = []
+  # each_item(head) do |item|
+  #   array << item.val
+  # end
+  # return get_input(array.reverse)
+
+  # Solution 3 (best among the 1st 3 solutions)
+  # curser = head
+  # previous_curser = nil
+  # reversed_head = head
+
+  # loop do
+  #   break if curser.nil?
+  #   # puts "curser: #{curser}, previous_curser: #{previous_curser}, reversed_head: #{reversed_head}"
+  #   reversed_head = curser
+  #   next_curser = curser.next
+  #   reversed_head.next = previous_curser
+  #   previous_curser = curser
+  #   curser = next_curser
+  # end
+  # return reversed_head
+
+
+  # Solution 4 (copied)
+  array = []
+  each_item(head) do |item|
+    array << item.val
+  end
+  node = head
+  while(array.any?) do
+    node.val = array.pop
+    node = node.next
+  end
+  return head
+end
+
+def reverse_list_recursive(head)
+  # puts "Current head: #{head.val}"
+  next_element = head.next
+
+  # since this is going to be the last element, remove the next value
+  head.next = nil
+
+  if next_element
+    new_head, new_tail = reverse_list_recursive(next_element)
+    # puts "received partials: #{new_head}, #{new_tail}"
+    # attaching the old head as the last item
+    new_tail.next = head
+
+    # puts "new_head: #{new_head.val}, new_tail: #{new_tail.next.val}"
+    # new_tail.next will become the new tail as the last element is the tail
+    # return new_head, new_tail
+    return new_head, new_tail.next
   else
-    puts "last element: #{head.val}"
-    return head
+    # puts "last element: #{head.val}"
+    # puts "new_head: #{head.val}, new_tail: #{head.val}"
+    # new_head = head
+    # new_tail = head
+    # return new_head, new_tail
+    return head, head
   end
 end
 
@@ -31,19 +98,19 @@ def each_item item
   end
 end
 def get_input array
-  input = nil
+  head = nil
   last_item = nil
   array.each do |i|
     new_item = ListNode.new(i, nil)
-    if input.nil?
-      input = new_item
+    if head.nil?
+      head = new_item
     else
       last_item.next = new_item
     end
     last_item = new_item
   end
-  input
+  head
 end
 
-reverse_list(get_input([1,2,3,4,5])).ps
+reverse_list(get_input([1,2,3])).ps
 
